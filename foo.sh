4f046628b52b3cacb869f674b90c68183f7fc40d
Sandro Manke <sandro@Dark-Silver.local>
2019-08-12 14:14:39 +0200

    Revert "fix"
    This reverts commit 5981be0712a45b5cf637e1f43c6c354272d86c7d.

--- foo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-aws cloudformation create-stack --stack-name smsmpipeline2 \
-    --template-body file:///Users/sandro/projects/allcloud/sagemaker-pipeline/CodePipeline/pipeline.yaml \
-    --parameters ParameterKey=Email,ParameterValue="sandro.manke@allcloud.io" ParameterKey=GitHubToken,ParameterValue="5ba36a4e97bb165ca5b337dad8db1d71a4c4d9b4" ParameterKey=GitHubUser,ParameterValue="sandrom-allcloud" \
-    --capabilities CAPABILITY_NAMED_IAM \
-    --profile sandro-sandbox \
-    --region us-east-1
